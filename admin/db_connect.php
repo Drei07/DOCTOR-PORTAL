fa9f2f05f74865722b04c953d8d23c5fe8692c58
unknown <you@example.com>
2022-10-25 20:23:32 +0800

    MAIN

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','doctors_appointment_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','u867039073_doctor_portal','Doctorportal2022','u867039073_doctor_portal')or die("Could not connect to mysql".mysqli_error($con));
